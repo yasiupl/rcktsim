9468c8d72d2d397bfa7ee555357e7b51e3dc5a66
yasiupl <contact@yasiu.pl>
2019-10-29 19:30:44 +0100

    moving ball demo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,52 @@
 #include <SFML/Graphics.hpp>
 
+class Sprite {
+    private:
+        sf::CircleShape shape;
+        float x_vel, y_vel = 0;
+        
+    public:
+    Sprite(float x, float y, float size) {
+        shape.setRadius(size);
+        shape.setFillColor(sf::Color::Green);
+        shape.setPosition(x, y);
+    };
+
+    sf::Vector2f getPostion() {
+        return shape.getPosition();
+    } 
+
+    void setVelocityX(float x) {
+        x_vel = x;
+    } 
+
+    void setVelocityY(float y) {
+        y_vel = y;
+    } 
+
+    void setVelocity(float x, float y) {
+        x_vel = x;
+        y_vel = y;
+    } 
+
+    void animate() {
+        sf::Vector2f postion = getPostion();
+        shape.setPosition(postion.x + x_vel, postion.y + y_vel);
+    }
+
+    sf::CircleShape getDrawable() {
+        return shape;
+    }
+};
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "rcktsim 0.0.0b1");
+    window.setFramerateLimit(60);
+    
+    Sprite Circle(100, 100, 50.f);
+    
+    float speed = 10;
 
     while (window.isOpen())
     {
@@ -15,10 +57,30 @@ int main()
                 window.close();
         }
 
+        Circle.setVelocity(0,0);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            Circle.setVelocityY(-1 * speed);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+            Circle.setVelocityY(1 * speed);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            Circle.setVelocityX(-1 * speed);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
+            Circle.setVelocityX(1 * speed);
+        }
+
+        Circle.animate();
         window.clear();
-        window.draw(shape);
+        window.draw(Circle.getDrawable());
         window.display();
     }
 
     return 0;
-}
+};
