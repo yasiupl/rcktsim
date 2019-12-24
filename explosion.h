2064dac2b2759703d385d6c2c2a5f533a77a7938
yasiupl <contact@yasiu.pl>
2019-12-24 13:41:54 +0100

    add working explosion sprite animation

--- explosion.h
+++ explosion.h
@@ -3,28 +3,34 @@
 
 class Explosion : public Entity {
 	protected:
-		sf::IntRect rectangle(0, 0, 96, 96);
+		sf::IntRect rectangle;
 
 	public:
-		Explosion(sf::Vector2f _position, float _rotation, sf::Vector2f _bbox, sf::RenderTarget* _target, std::vector<Entity*> *_renderQueue) : Entity(_position, _rotation - 90.f, _bbox, "explosion.png", 0.1, _target, _renderQueue) {
-			sprite.setOrigin(texture.getSize().x /2, texture.getSize().y /2);
+		Explosion(sf::Vector2f _position, float _rotation, sf::Vector2f _bbox, sf::RenderTarget* _target, std::vector<Entity*> *_renderQueue) : Entity(_position, _rotation - 90.f, _bbox, "explosion.png", 1.0, _target, _renderQueue), rectangle(0, 0, 96, 96) {
+			
 			sprite.setTextureRect(rectangle);
+			sprite.setOrigin(48, 48);
 	}
 
 	void animate() {
-		animationTime = animationTimer.getElapsedTime();
-		if (animationTime > 50.0f) {
-			if (rectangle.left >= 5*96)
+		if(exploded == false) {
+			animationTime = animationTimer.getElapsedTime();
+		if (animationTime.asMilliseconds() >= 50.0f) {
+			if (rectangle.left >= 4*96) {
 				rectangle.left = 0;
+				rectangle.top += 96;
+			}
 			else 
 				rectangle.left += 96;
 		
-			if (rectangle.top >= 4*96)
-				rectangle.top = 0;
-			else 
-				rectangle.top += 96;
+			if (rectangle.top >= 4*96) {
+				stop();
+			}
 			
+			
+			sprite.setTextureRect(rectangle);
 			animationTimer.restart();
+		}
 		}
 	}
 };
